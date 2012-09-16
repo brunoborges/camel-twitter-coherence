6c820ab7cba192fc0ab715c94702274518e20785
Bruno Borges <bruno.borges@gmail.com>
2012-09-16 04:40:37 -0300

    versao quase pronta pra demo

--- src/main/scala/demo/javaone/camelcoherence/DemoRouteBuilder.scala
+++ src/main/scala/demo/javaone/camelcoherence/DemoRouteBuilder.scala
@@ -4,33 +4,45 @@ import org.apache.camel.Exchange
 import org.apache.camel.scala.dsl.builder.RouteBuilder
 import org.apache.camel.component.twitter.TwitterComponent
 import com.oracle.coherence.components.camel.CacheConstants
+import twitter4j.Status
+import org.apache.camel.processor.aggregate.UseLatestAggregationStrategy
 
 class DemoRouteBuilder extends RouteBuilder with ConfigureTwitterComponent {
 
   Statistics.keywords = configProperties.getProperty("twitter.searchTerm")
-  Statistics.keywords = "photo"
 
-  /*
-  val tc = new TwitterComponent()
-  tc.setAccessToken("14566379-pXIfEE24ByIwOUq1jHwR2CubGlDUIN0q9OWsc9iU")
-  tc.setAccessTokenSecret("ZpEcg1uyNvZdUA1gFAjqs7Ld22DjjExTOJHNtddU")
-  tc.setConsumerKey("6KpGgI631s4RanmOB3qbg")
-  tc.setConsumerSecret("pYDxMF69eUM49PCenQz374x5HeEY8I9Tx75jFvL3U")
-  */
-
-  //getContext.addComponent("twitter", tc)
-
-  "twitter://streaming/sample?type=event" ==> {
-	  to("log:output")
-	  setHeader(CacheConstants.CACHE_OPERATION, CacheConstants.CACHE_OPERATION_ADD)
-	  setHeader(CacheConstants.CACHE_KEY, "lastTweet")
-	  to("coherence:tweets")
+  "twitter://streaming/sample?type=event&keywords=batman" ==> {
+    when(_.in.asInstanceOf[Status].getMediaEntities() != null) {
+      setHeader("UNIQUE_IMAGE_URL", _.in.asInstanceOf[Status].getMediaEntities()(0).getMediaURL().toString())
+      to("direct:tweetsWithImage")
+    }
   }
-/*  
-  "twitter://streaming/sample?type=event" +
-  "&accessToken=14566379-pXIfEE24ByIwOUq1jHwR2CubGlDUIN0q9OWsc9iU" +
-  "&accessTokenSecret=ZpEcg1uyNvZdUA1gFAjqs7Ld22DjjExTOJHNtddU" +
-  "&consumerKey=6KpGgI631s4RanmOB3qbg" +
-  "&consumerSecret=pYDxMF69eUM49PCenQz374x5HeEY8I9Tx75jFvL3U" to "log:output"
-*/
+
+  "direct:tweetsWithImage" ==> {
+    to("direct:checkCache")
+    when(_.getIn().getHeader(CacheConstants.CACHE_ELEMENT_WAS_FOUND) == null) {
+      to("direct:storeOnCache")
+      to("direct:aggregate")
+    }
+  }
+
+  "direct:checkCache" ==> {
+    setHeader(CacheConstants.CACHE_OPERATION, CacheConstants.CACHE_OPERATION_CHECK)
+    setHeader(CacheConstants.CACHE_KEY, _.header("UNIQUE_IMAGE_URL"))
+    to("coherence:tweets")
+  }
+
+  "direct:storeOnCache" ==> {
+    setHeader(CacheConstants.CACHE_OPERATION, CacheConstants.CACHE_OPERATION_ADD)
+    setHeader(CacheConstants.CACHE_KEY, _.header("UNIQUE_IMAGE_URL"))
+    to("coherence:tweets")
+  }
+
+  "direct:aggregate" ==> {
+    aggregate(_.in.asInstanceOf[Status].getId() > 0, new UseLatestAggregationStrategy()).completionInterval(1000).to("direct:publish")
+  }
+  
+  "direct:publish" ==> {
+    to("log:output")
+  }
 }
